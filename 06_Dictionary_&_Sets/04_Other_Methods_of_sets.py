ddbd607e617be672e4560d9d444da7e702771266
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-06-02 23:02:51 +0530

    Add files via upload

---
+++ 06_Dictionary_&_Sets/04_Other_Methods_of_sets.py
@@ -0,0 +1,13 @@
+# union() and intersection()
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3,"a"}
+set3=set1.intersection(set2)
+print(set3)
+
