f342db5a978aa78e5abf1c2b54c3e8cf59fbf2ba
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-06-18 23:56:29 +0530

    Add files via upload

---
+++ 07_Conditional Expressions/03_Short_hand.py
@@ -0,0 +1,14 @@
+# short hand if
+a=23
+b=4
+
+if a > b: print("a is greater than b")
+
+# short hand if
+print("a is greater ") if a > b else print("b is greater ")
+
+
+#pass statements 
+b=300
+if b > a:
+  pass
