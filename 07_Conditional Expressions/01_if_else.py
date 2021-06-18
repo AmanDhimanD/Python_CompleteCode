4b206eaf0b11e1dff5aa6cdac77cd6849d7e294b
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2021-06-18 23:55:58 +0530

    Add files via upload

---
+++ 07_Conditional Expressions/01_if_else.py
@@ -0,0 +1,23 @@
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Less than or equal to: a <= b
+# Greater than: a > b
+# Greater than or equal to: a >= b
+
+a=12
+b=11
+if a>b:
+    print(f"a is greater than b by {a-b}")
+else:
+    print(f"a is smaller than b by {b-a}")
+
+
+# Indentation is important for condition Experssions
+""" 
+if a>b:
+print(f"a is greater than b by {a-b}")
+else:
+print(f"a is smaller than b by {b-a}") 
+"""
+# NOTE: THIS IS WRONG INDENTATION
