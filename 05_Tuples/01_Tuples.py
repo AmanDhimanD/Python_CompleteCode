555e606de76c85fb3ce4bf5d8a58e1171886d404
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2022-03-11 22:33:10 +0530

    Update 01_Tuples.py

--- 05_Tuples/01_Tuples.py
+++ 05_Tuples/01_Tuples.py
@@ -7,5 +7,8 @@ print(tup)
 a= (1,2,3,4,5,6,7,8,9,0) #Tuples
 b= [1,2,3,4,5,6,7,8,9,0] #Lists
 
-print('a=',a.__sizeof__())
-print('b=',b.__sizeof__())  
+print('a=',a.__sizeof__()) #104 Byte
+print('b=',b.__sizeof__()) #120 Byte
+# each elements are take 
+# Tuple - 28 bytes
+# List - 48 bytes
